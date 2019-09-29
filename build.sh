496c6d52a9015a057e036ce93ba8ba76c705b1c9
andy <andydavies2020@gmail.com>
2019-09-29 13:23:43 +0300

    version: use git tags for the release versions

--- build.sh
+++ build.sh
@@ -4,9 +4,18 @@ export GO111MODULE=on
 
 git_commit=$(git rev-parse HEAD)
 git_dirty=$(git status --porcelain)
+git_version=$(git tag -l --points-at HEAD | grep 'version/' | sed 's/version\/\(.*\)/\1/g' | sort | tail -n 1)
+
 git_dirty=${git_dirty:+"+CHANGES"}
 
-ldflags="-X xq/version.GitCommit=$git_commit$git_dirty"
+dev_tag=''
+
+if [ "$git_version" == "" ]; then
+  dev_tag="dev"
+  git_version="0.0.0"
+fi
+
+ldflags="-X xq/version.GitCommit=$git_commit$git_dirty -X xq/version.VersionPrerelease=$dev_tag -X xq/version.Version=$git_version"
 
 go get -v -d .
 go build \
